10fa12a04d38698bd6945ab2a5c99d88c7c5f7c7
FerminCorredera <fermincorredera@gmail.com>
2023-10-26 17:54:29 -0300

    arreglos pre entrega 3

--- main.js
+++ main.js
@@ -1,98 +1,138 @@
 
-
-//alert ("llamar a la funcion")
-
 const Persona = function (nombre, edad, area, sueldo) {
-    this.nombre = nombre
-    this.edad = edad
-    this.area = area
+  this.nombre = nombre
+  this.edad = edad
+  this.area = area
     this.sueldo = sueldo
-}
-//LISTA DEL PERSONAL
+ }
 
-let persona1 = new Persona ("Franco", 21, "Cocina", 1000)
-let persona2 = new Persona ("Kevin", 19, "Cocina", 1000)
-let persona3 = new Persona ("Juan", 31, "Barra", 700)
-let persona4 = new Persona ("Gabriel", 22, "Barra", 700)
-let persona5 = new Persona ("Milagros", 27, "Mozo", 900)
-let persona6 = new Persona ("Federico", 26, "Mozo", 900)
-let persona7 = new Persona ("Sofia", 20, "Commise", 500)
-let persona8 = new Persona ("Ailen", 27, "Commise", 500)
-let persona9 = new Persona ("Fermin", 23, "Deposito",800)
-let persona10 = new Persona ("Brian", 22, "Deposito",800)
-let persona11 = new Persona ("Jhon", 30, "Limpieza", 400)
-let persona12 = new Persona ("Marisa", 50, "Limpieza",400)
+ let persona1 = new Persona ("Franco", 21, "Cocina", 1000)
+ let persona2 = new Persona ("Kevin", 19, "Cocina", 1000)
+ let persona3 = new Persona ("Juan", 31, "Barra", 700)
+ let persona4 = new Persona ("Gabriel", 22, "Barra", 700)
+ let persona5 = new Persona ("Milagros", 27, "Mozo", 900)
+ let persona6 = new Persona ("Federico", 26, "Mozo", 900)
+ let persona7 = new Persona ("Sofia", 20, "Commise", 500)
+ let persona8 = new Persona ("Ailen", 27, "Commise", 500)
+ let persona9 = new Persona ("Fermin", 23, "Deposito",800)
+ let persona10 = new Persona ("Brian", 22, "Deposito",800)
+ let persona11 = new Persona ("Carlos", 30, "Limpieza", 400)
+ let persona12 = new Persona ("Marisa", 50, "Limpieza",400)
+ 
+ let lista = [persona1, persona2,persona3,persona4,persona5,persona6,persona7,persona8,persona9,persona10,persona11,persona12]
 
-//array del personal
 
-let lista = [persona1, persona2,persona3,persona4,persona5,persona6,persona7,persona8,persona9,persona10,persona11,persona12]
+const buttonfiltrar = document.getElementById("filtrar");
+buttonfiltrar.addEventListener("click", filtrarPersonal);
 
-//filtro sin espacios y en minuscula del area donde trabaja el empleado
+const agregar = document.getElementById("agregar");
+agregar.addEventListener('click', cargaEmpleado);
 
-function filtrarPersonal (){
-    let palabraClave = prompt("Ingresa dato del empleado").trim().toLowerCase()
-    let resultado = lista.filter((x)=> x.area.toLowerCase().includes(palabraClave))
-    if(resultado.length >0){
-        console.table("hay " + resultado.length + " " + palabraClave)
-    }else{
-        alert("no hay resultado con " + palabraClave)
-    }
-}
+function filtrarPersonal() {
+    const section = document.querySelector("section");
+    const input = document.getElementById("filtroPersonal").value
+    const palabraClave = input.trim().toUpperCase();
+    const resultado = lista.filter((persona) => persona.nombre.toUpperCase().includes(palabraClave));
 
-//agrego los datos de un nuevo empleado
-function nuevaPersona() {
-    let nombre = ""
-    let edad = ""
-    let area = ""
-    let sueldo
-   // let lista = []
 
-    while(nombre != "no"){
-        nombre = prompt ("ingresa el nombre del empleado, ingrese No para cortar").trim(). toLowerCase()
-        edad = parseInt(prompt("ingresa la edad del empleado"))
-        area = prompt ("ingresa la nueva area de trabajo").trim(). toLowerCase()
-        sueldo = parseFloat(prompt("ingresa el sueldo percibido")) 
+if (resultado.length >0){
+    const container = document.createElement('div');
+     
+ 
+resultado.forEach((persona) => { 
+  const card = document.createElement("div");
     
-    //verificacion si los datos ingresados estan bien 
-        if (isNaN(sueldo) || isNaN(edad) ||nombre === "" || area === "") {
-            alert ("por favor ingrese los datos correctos")
-        }
-        else{
-            console.log("entra")
-            let persona = new Persona (nombre,edad,area,sueldo)
-            lista.push(persona)
-        }
-    }
+  const nombre = document.createElement("h3");
+  nombre.textContent = persona.nombre;
+  card.appendChild(nombre);
 
-  //  return lista
+  const edad = document.createElement("p");
+  edad.textContent = `edad: ${persona.edad}`;
+  card.appendChild(edad);
+
+    const area = document.createElement("p");
+  area.textContent = `area: ${persona.area}`;
+  card.appendChild(area);
+
+  const sueldo = document.createElement("p");
+  sueldo.textContent = `sueldo: ${persona.sueldo}`;
+  card.appendChild(sueldo);
+
+
+
+  
+  container.appendChild(card);
+});
+
+section.appendChild(container);
+
 }
-//funcion para calcular distintos datos del personal, en este caso la de edad de los mozos
-function calcularDatos(lista) {
-
-    let promedio = 0
-    let suma = 0
-    let cont = 0
-
-    for (let i = 0; i<lista.length; i++) {
-
-        if(lista[i].area.toLowerCase() == "mozo"){
-            suma += lista[i].edad  
-            console.log ("suma: " + suma)
-            cont++
-            console.log ("cont " + cont )
-        }
-    }
-
-    promedio = suma/cont
-    console.log("El promedio de edad de los mozos es de " + promedio)
-} 
+else {
+  alert('No se encontro ningun empleado con ese nombre');
+}
+}
 
 
-//let lista = nuevaPersona()
-nuevaPersona()
-calcularDatos(lista)
-filtrarPersonal()
+function cargaEmpleado() {
+   let nombre = document.getElementById("nombre").value.trim();
+   let edad = parseFloat(document.getElementById("edad").value);
+   let area = document.getElementById("area").value.trim();
+   let sueldo = parseFloat(document.getElementById("sueldo").value);
+   
+   if (isNaN(edad) || isNaN(sueldo) || nombre === "" || area === '') { 
+    alert('Por favor ingresa valores válidos.');
+    return;
+  }
+ 
+   let persona = new Persona (nombre,edad,area,sueldo);
+   console.log(persona)
+
+  lista.push(persona);
+  console.log("tamano lista: ", lista.length)
+  console.log(lista)
+  document.getElementById("nombre").value = '';
+  document.getElementById("edad").value = '';
+  document.getElementById("area").value = '';
+  document.getElementById("sueldo").value = '';
+
+  muestraLista(lista)
+}
 
 
-//agrego al nuevo empleado al sistema
-//console.table (lista)
+
+
+
+
+function muestraLista (lista){
+  const caja = document.createElement("div");
+  lista.forEach((persona) => {   
+
+  const card = document.createElement('div'); 
+  const nombre = document.createElement('h3');
+  nombre.textContent = persona.nombre;
+  card.appendChild(nombre);
+
+  const edad = document.createElement('p');
+  edad.textContent = `Edad: ${persona.edad}`;
+  card.appendChild(edad);
+
+  const area = document.createElement('p');
+  area.textContent = `Area: ${persona.area}`;
+  card.appendChild(area);
+
+  const sueldo = document.createElement('p');
+  sueldo.textContent = `Sueldo: ${persona.sueldo}`;
+  card.appendChild(sueldo);
+
+  caja.appendChild(card);
+  const body = document.querySelector('body');
+  body.appendChild(caja);
+});
+}
+
+
+
+
+
+
+
